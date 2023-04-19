708fb71635513bcb0192a223384fa14a3f9b9c03
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-04-18 21:39:08 -0400

    assertions part 1

--- src/main/java/com/edgar/unittest101/DemoUtils.java
+++ src/main/java/com/edgar/unittest101/DemoUtils.java
@@ -1,6 +1,15 @@
 package com.edgar.unittest101;
 
+import java.util.List;
+
 public class DemoUtils {
+
+    private String academy ="LuvYouMummy";
+    private String academyDuplicate = academy;
+
+    private String [] firstThreeLettersOfAlphabet={"A","B","C"};
+
+    private List<String> luvInList = List.of("Luv","You","Mummy");
     
     public Object checKNull(Object obj){
         return obj !=null ? obj : null;
@@ -8,5 +17,25 @@ public class DemoUtils {
 
     public int add(int x , int y){
         return x+y;
+    }
+
+    public Boolean isGreater( int n1, int n2){
+        return n1 > n2;
+    }
+
+    public String getAcademy(){
+        return academy;
+    }
+
+    public String getAcademyDuplicate(){
+        return academyDuplicate;
+    }
+
+    public String[] getfirstThreeLettersOfAlphabet(){
+        return firstThreeLettersOfAlphabet;
+    }
+
+    public List<String> getLuvInList(){
+        return luvInList;
     }
 }
--- src/test/java/com/edgar/unittest101/DemoUtilsTest.java
+++ src/test/java/com/edgar/unittest101/DemoUtilsTest.java
@@ -1,22 +1,34 @@
 package com.edgar.unittest101;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
 public class DemoUtilsTest {
 
     DemoUtils demoUtils ;
 
     @BeforeAll
-    static void setUpbeforeAll(){
+    static void setUpBeforeAll(){
         System.out.println("executes before all, must be static");
     }
 
@@ -51,5 +63,40 @@ public class DemoUtilsTest {
         assertEquals(6, demoUtils.add(4, 2), "2+4 equals 6");
         assertNotEquals(8, demoUtils.add(5, 5), "not equal 5+5 is 10");
 
+    }
+
+    @Test
+    void testTrueOrFalse(){
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo),"This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @Test
+    void testSameAndNotSame(){
+        String str = "LuvYouMummy Alot";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to the same object");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects are not the same");
+    }
+
+    @Test
+    void testArrayEquals(){
+        String[] stringArray = {"A","B","C"};
+        assertArrayEquals(stringArray, demoUtils.getfirstThreeLettersOfAlphabet(), "Arrays Should be the same");
+    }
+
+    @Test
+    void testIterableEquals(){
+        List<String> theList = List.of("Luv","You","Mummy");
+        assertIterableEquals(theList, demoUtils.getLuvInList(), "Iterable should be the same");
+    }
+
+    @Test
+    void testLinesMatch(){
+        List<String> theList = List.of("Luv","You","Mummy");
+        assertLinesMatch(theList, demoUtils.getLuvInList(), "Lines should match");
     }
 }
