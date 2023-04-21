06f3b906a04622eddfaefb3b2f82a02ea580cc22
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-04-20 23:57:18 -0400

    tdd started

---
+++ src/test/java/com/edgar/tdd/FizzBuzzTest.java
@@ -0,0 +1,25 @@
+package com.edgar.tdd;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FizzBuzzTest {
+    
+    /*
+     * if number is divisible by 3, print Fizz
+     * if number is divisible by 5, print Buzz
+     * if number is divisible by 3 and 5, print FizzBuzz
+     * if number is NOT divisible by 3 or 5, then print the number
+     */
+    @DisplayName("Divisble by 3")
+    @Test
+    @Order(1)
+     void testFoDivisibleByThree(){
+        fail("fail");
+     }
+}
